8a2659e76c00caf0822904b46d7ee940cd4c7b95
babusikara-debug <babusikara@gmail.com>
2026-07-28 19:48:55 +0530

    1.4

--- SplitwiseApp.java
+++ SplitwiseApp.java
@@ -6,12 +6,14 @@ public class SplitwiseApp
         System.out.println("=== SplitWise ===");
         String payerName = "Riya";
         double totalAmount = 500;
-        int num = 3;
+        int numFriends = 3;
+        double perPersonShare = totalAmount/numFriends;
         System.out.println("Track shared expenses with friends.");
         System.out.println();
         System.out.println("Ready. More features coming in the next lessons");
         System.out.println(payerName);
         System.out.println(totalAmount);
-        System.out.println(num);
+        System.out.println(numFriends);
+        System.out.println(perPersonShare);
     }
 }
