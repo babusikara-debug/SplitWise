5374b97f6ec96f136ec058a57ab6e6a0b59fc9fe
babusikara-debug <babusikara@gmail.com>
2026-07-28 18:11:39 +0530

    1.1

---
+++ SplitwiseApp.java
@@ -0,0 +1,12 @@
+package SplitWise;
+public class SplitwiseApp
+{
+    public static void main(String[] args)
+    {
+        System.out.println("=== SplitWise ===");
+        System.out.println("Track shared expenses with friends.");
+        System.out.println();
+        System.out.println("Ready. More features coming in the next lessons");
+
+    }
+}
